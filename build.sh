0bc82274ea0121c1c811b6bad96e4859115f2351
serhii.n <serhii.n@thescimus.com>
2022-10-03 22:07:50 +0300

    Update kernel to 6.0

--- build.sh
+++ build.sh
@@ -2,24 +2,22 @@
 
 set -e
 
-LINUX_VERSION=5.18.3
+LINUX_VERSION=6.0
 
 apt update
 apt full-upgrade -y
 
 apt install build-essential linux-source bc kmod cpio flex bison libelf-dev libssl-dev libncurses5-dev rsync python3 dwarves wget git -y
 
-cd /tmp
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
-rm -rf /lib/firmware
-mv /tmp/linux-firmware /lib/firmware
-
 cd /root/build
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$LINUX_VERSION.tar.xz -O - | tar xJ
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$LINUX_VERSION.tar.xz -O - | tar xJ
 
 cp -v /root/build/config /root/build/linux-$LINUX_VERSION/.config
 cd /root/build/linux-$LINUX_VERSION
 patch -p1 < /root/build/patches/0001-Revert-iwlwifi-remove-lar_disable-module-parameter.patch
+make oldconfig
+scripts/config --disable SECURITY_LOCKDOWN_LSM
+scripts/config --disable MODULE_SIG
 make -j$(nproc) bindeb-pkg LOCALVERSION=-my KDEB_PKGVERSION=$(make kernelversion)-1 ARCH=x86_64
 cd /root/build/
 rm -rf /root/build/linux-$LINUX_VERSION
